1d853a7a3c948af32e8972eb244a3d40859bff53
Vivek Raskar <vivek.raskar@kissht.com>
2019-12-11 15:40:31 +0530

    login functionality added

--- src/api/api-user-login.ts
+++ src/api/api-user-login.ts
@@ -3,6 +3,7 @@ const expressRouter = Router();
 import {config} from '../config';
 import * as bcrypt from 'bcrypt';
 import {Database} from '../models/database';
+import * as jwt from 'jsonwebtoken';
 //import {ObjectID} from 'mongodb';
 
 // Validations Middleware
@@ -12,6 +13,53 @@ export class ApiLoginSignup{
     dataRouter:any; 
     constructor(){
         this.dataRouter = expressRouter;
+
+        this.dataRouter.get("/login",
+        ValidationLogin.loginValidator(), 
+        ValidationErrorHandlder.handleErrors, 
+        async (req:Request, res:Response) => {            
+            const readParams = {
+                collection : 'users',
+                criteria : { 'email': req.body.email},
+                projection : {}
+            }
+            try{
+                const docs: any = await new Database().readOne(readParams);
+                if(docs !== null){
+                    if(bcrypt.compareSync(req.body.password, docs.password)) {
+                        
+                        const token = jwt.sign( { email : docs.email },
+                                                config.get('jwtSecretKey'), 
+                                                { expiresIn: '1h' });
+
+                        return res.send({
+                            status : true,
+                            message : "User details found",
+                            data : {
+                                user : docs,
+                                token : token
+                            }
+                        });
+
+                    } else {
+                        return res.send({
+                            status : false, 
+                            message : "Invalid Mobile/password!!!", 
+                            data : null
+                        });
+                    }
+
+                } else {
+                    return res.send({
+                        status : false,
+                        message : "User not found!..",
+                        data : null
+                    });
+                }
+            } catch(e){
+                res.status(500).send(`${e.message}-${e.stack}`);
+            }
+        })
 
         this.dataRouter.post("/signup", 
         ValidationLogin.signupValidator(), 
@@ -28,10 +76,9 @@ export class ApiLoginSignup{
                 projection      : {}
             }
             try{
-                const docs = await new Database().write(writeParams);
+                const docs: any = await new Database().write(writeParams);
                 res.send(docs)
             } catch(e){
-                console.error(`${e.message}-${e.stack}`)
                 res.status(500).send(`${e.message}-${e.stack}`);
             }
         });
--- tests/integration/api-spec.ts
+++ tests/integration/api-spec.ts
@@ -3,12 +3,22 @@ import {app} from '../../src/app'
 
 describe("API Specs Test Suite", ()=>{
 
+    test("/GET /Login should return login user details", async done=>{
+        const response = await request(app).get("/auth/login").send({
+            "email" : "vivek001@kissht.com",
+            "password" : "12345"
+        })
+        const {status,body} = response
+        expect(status).toEqual(200)
+        done()
+    })
+
     test("/POST /Signup should create user", async done=>{
         const response = await request(app).post("/auth/signup").send({
             "first_name" : "Vivek",
             "last_name"	 : "R",
-            "email"		 : "vive31@ras.com",
-            "mobile"	 : "9867254450",
+            "email"		 : "vivek@dbit.com",
+            "mobile"	 : "9867000000",
             "password"	: "12345"
         })
         const {status,body} = response
